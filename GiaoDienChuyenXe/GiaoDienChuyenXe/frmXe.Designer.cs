e8376f9b86e64f4c95d6409fd12b2b96ed47f6f5
Dương Hoàng Trung <44616927+1660661@users.noreply.github.com>
2018-11-11 16:14:06 +0700

    Add From GiaoDienChuyenXe

---
+++ GiaoDienChuyenXe/GiaoDienChuyenXe/frmXe.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GiaoDienChuyenXe
+{
+    partial class frmXe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvXe = new System.Windows.Forms.DataGridView();
+            this.grXe = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvXe)).BeginInit();
+            this.grXe.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvXe
+            // 
+            this.dgvXe.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(222)))), ((int)(((byte)(251)))));
+            this.dgvXe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvXe.Location = new System.Drawing.Point(1, 108);
+            this.dgvXe.Name = "dgvXe";
+            this.dgvXe.RowTemplate.Height = 24;
+            this.dgvXe.Size = new System.Drawing.Size(780, 350);
+            this.dgvXe.TabIndex = 0;
+            // 
+            // grXe
+            // 
+            this.grXe.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.grXe.Controls.Add(this.label1);
+            this.grXe.Location = new System.Drawing.Point(1, 1);
+            this.grXe.Name = "grXe";
+            this.grXe.Size = new System.Drawing.Size(780, 100);
+            this.grXe.TabIndex = 1;
+            this.grXe.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(98)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(260, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông Tin Xe";
+            // 
+            // frmXe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.grXe);
+            this.Controls.Add(this.dgvXe);
+            this.Name = "frmXe";
+            this.Text = "frmXe";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvXe)).EndInit();
+            this.grXe.ResumeLayout(false);
+            this.grXe.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvXe;
+        private System.Windows.Forms.GroupBox grXe;
+        private System.Windows.Forms.Label label1;
+    }
+}
