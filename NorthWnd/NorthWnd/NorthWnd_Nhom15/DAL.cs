39e1ad59832eca6aaf42076da1decc048c162f7e
1660765 <1660765@student.hcmus.edu.vn>
2018-11-12 23:26:56 +0700

    added BLL, DAL, form Edit

--- NorthWnd/NorthWnd/NorthWnd_Nhom15/DAL.cs
+++ NorthWnd/NorthWnd/NorthWnd_Nhom15/DAL.cs
@@ -3,11 +3,145 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace NorthWnd_Nhom15
 {
+    // connect SQL and execute Query
     class DAL
     {
-        // connect SQL and execute Query
+        // connect string
+        string strConnect = @"Server=ROOT\SQLEXPRESS;Database=NORTHWND;Integrated Security=SSPI";
+        SqlConnection conn = null;
+
+        // connect to database
+        public void Connect()
+        {
+            try
+            {
+                if (conn == null)
+                {
+                    conn = new SqlConnection(strConnect);
+                }
+
+                if (conn.State != ConnectionState.Closed)
+                {
+                    conn.Close();
+                }
+
+                conn.Open();
+            } catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // try dis connect form database
+        public void DisConnected()
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        // execute non query
+        public int ExecuteNonQuery(CommandType cmdType, string sqlStr )
+        {
+            try
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = cmdType;
+                cmd.CommandText = sqlStr;
+
+                int row = cmd.ExecuteNonQuery();
+                return row;
+            } catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // execute non query with parameter
+        public int ExecuteNonQuery(CommandType cmdType, string sqlStr, params SqlParameter[] parameters)
+        {
+            try
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = cmdType;
+                cmd.CommandText = sqlStr;
+                
+                if (parameters != null && parameters.Length > 0)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+
+                int row = cmd.ExecuteNonQuery();
+                return row;
+            } catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // execute data reader
+        public SqlDataReader GetReader(CommandType cmdType, string sqlStr)
+        {
+            try
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = cmdType;
+                cmd.CommandText = sqlStr;
+
+                return cmd.ExecuteReader();
+
+            } catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //excute data reader with parameter
+        public SqlDataReader GetReader(CommandType cmdType, string sqlStr, params SqlParameter[] parameters)
+        {
+            try
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = cmdType;
+                cmd.CommandText = sqlStr;
+
+                if (parameters != null && parameters.Length > 0)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+
+                return cmd.ExecuteReader();
+            } catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //data table
+        public DataTable SelectAll(CommandType cmdType, string strSql)
+        {
+            try
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = cmdType;
+                cmd.CommandText = strSql;
+
+                SqlDataAdapter ad = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                ad.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
--- NorthWnd/NorthWnd/NorthWnd_Nhom15/frmEdit.Designer.cs
+++ NorthWnd/NorthWnd/NorthWnd_Nhom15/frmEdit.Designer.cs
@@ -28,34 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // gridView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(1, 175);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(797, 274);
-            this.dataGridView1.TabIndex = 0;
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridView.Location = new System.Drawing.Point(0, 0);
+            this.gridView.MultiSelect = false;
+            this.gridView.Name = "gridView";
+            this.gridView.Size = new System.Drawing.Size(1264, 449);
+            this.gridView.TabIndex = 0;
             // 
             // frmEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1264, 449);
+            this.Controls.Add(this.gridView);
             this.Name = "frmEdit";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Edit - Suppliers - NorthWnd";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView gridView;
     }
 }
 
