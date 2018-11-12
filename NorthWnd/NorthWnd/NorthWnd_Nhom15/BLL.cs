b393cf25faa25f4eca3c217333fc4db66e23ec90
1660765 <1660765@student.hcmus.edu.vn>
2018-11-12 14:50:31 +0700

    init DAL, BLL class in NorthWnd project

---
+++ NorthWnd/NorthWnd/NorthWnd_Nhom15/BLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWnd_Nhom15
+{
+    class BLL
+    {
+        public void Edit(List<string> data)
+        {
+            // check valid data and call DAL for update
+        }
+    }
+}
---
+++ NorthWnd/NorthWnd/NorthWnd_Nhom15/DAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWnd_Nhom15
+{
+    class DAL
+    {
+        // connect SQL and execute Query
+    }
+}
