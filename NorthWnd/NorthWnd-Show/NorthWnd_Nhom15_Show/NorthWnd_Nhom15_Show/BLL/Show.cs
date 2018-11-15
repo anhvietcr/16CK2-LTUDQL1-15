d0f8e1ff26d4879f04cf85b422a66dae11757e31
1660699 <44648446+1660699@users.noreply.github.com>
2018-11-16 02:13:57 +0700

    Created NorthWnd Show Solution

---
+++ NorthWnd/NorthWnd-Show/NorthWnd_Nhom15_Show/NorthWnd_Nhom15_Show/BLL/Show.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace NorthWnd_Nhom15_Show
+{
+    class BLLShow
+    {
+        private DAL dataProvider;
+        public BLLShow()
+        {
+            dataProvider = new DAL();
+            dataProvider.Connect();
+        }
+
+        //data table
+        public DataTable getDataTable()
+        {
+            try
+            {
+                string sqlSelect = "SELECT * FROM Suppliers";
+                DataTable dt = dataProvider.SelectAll(CommandType.Text, sqlSelect);
+
+                dataProvider.DisConnected();
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
