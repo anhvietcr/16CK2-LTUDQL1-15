ffb8a24739c90e67e5c680d96971c165702e7d05
1660765 <1660765@student.hcmus.edu.vn>
2018-11-24 20:22:43 +0700

    added data to database

--- THONG TIN DAT VE/QuanLyNhaXe/frmDashboard.Designer.cs
+++ THONG TIN DAT VE/QuanLyNhaXe/frmDashboard.Designer.cs
@@ -53,10 +53,8 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label6 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label9 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.btn_slider = new System.Windows.Forms.Button();
             this.panel_navbar_title = new System.Windows.Forms.Panel();
             this.ctr_navbar_title = new UserController.ctr_navbar_title();
@@ -304,10 +302,8 @@
             this.groupBox_infoteam.Controls.Add(this.pictureBox3);
             this.groupBox_infoteam.Controls.Add(this.label6);
             this.groupBox_infoteam.Controls.Add(this.pictureBox2);
-            this.groupBox_infoteam.Controls.Add(this.label9);
             this.groupBox_infoteam.Controls.Add(this.label3);
             this.groupBox_infoteam.Controls.Add(this.pictureBox1);
-            this.groupBox_infoteam.Controls.Add(this.label2);
             this.groupBox_infoteam.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox_infoteam.Location = new System.Drawing.Point(213, 98);
             this.groupBox_infoteam.Name = "groupBox_infoteam";
@@ -321,7 +317,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(77, 318);
+            this.label10.Location = new System.Drawing.Point(77, 333);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(56, 14);
             this.label10.TabIndex = 5;
@@ -331,11 +327,11 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(77, 289);
+            this.label11.Location = new System.Drawing.Point(76, 297);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(138, 18);
+            this.label11.Size = new System.Drawing.Size(88, 18);
             this.label11.TabIndex = 4;
-            this.label11.Text = "Phan Anh Việt";
+            this.label11.Text = "Anh Việt";
             // 
             // label8
             // 
@@ -351,7 +347,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(77, 147);
+            this.label4.Location = new System.Drawing.Point(77, 160);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 14);
             this.label4.TabIndex = 3;
@@ -361,11 +357,11 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(76, 197);
+            this.label5.Location = new System.Drawing.Point(76, 206);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(118, 18);
             this.label5.TabIndex = 2;
-            this.label5.Text = "Dương Hoàng";
+            this.label5.Text = "Hoàng Trung";
             // 
             // label7
             // 
@@ -373,9 +369,9 @@
             this.label7.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(77, 119);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(138, 18);
+            this.label7.Size = new System.Drawing.Size(108, 18);
             this.label7.TabIndex = 2;
-            this.label7.Text = "Lê Quang Vinh";
+            this.label7.Text = "Quang Vinh";
             // 
             // pictureBox4
             // 
@@ -419,21 +415,11 @@
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(99, 215);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(58, 18);
-            this.label9.TabIndex = 0;
-            this.label9.Text = "Trung";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(99, 44);
+            this.label3.Location = new System.Drawing.Point(77, 29);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(118, 18);
             this.label3.TabIndex = 0;
@@ -449,16 +435,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(76, 25);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 18);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Đặng Thị";
             // 
             // btn_slider
             // 
@@ -552,7 +528,6 @@
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
@@ -563,7 +538,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.ImageList imgListAvatar;
