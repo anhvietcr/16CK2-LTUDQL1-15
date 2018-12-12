6848bebd10e4ad68344d5bd2d1407507c7389c7d
1660699 <1660699@student.hcmus.edu.vn>
2018-12-12 20:24:10 +0700

    add Insert Tram,Tuyen, Delete Tuyen

---
+++ GiaoDienTuyenXe/DAL/DAL_Tram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DAL
+{
+    public class DAL_Tram
+    {
+        public DataTable listTenTram()
+        {
+
+            //tạo kết nối mới
+            SqlConnection conn = DBConnect.Connect();
+            //tạo command(select *....)
+            SqlCommand cmd = conn.CreateCommand();
+            //khai bao se dung store cho cmd
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "listTenTram";
+            //thuc thi cmd
+            //khi dung datatable hoac dataset thi moi dung SQLdataAdapter
+            //cmd.ExecuteNonQuery();//thuc thi tra ve so dong bi anh huong
+            SqlDataAdapter da = new SqlDataAdapter(cmd);//thuc thi tra ve table
+            DataTable dt = new DataTable();
+            //toàn bộ dữ liệu trong da se đổ (Fill) vao dt(dang table)
+            da.Fill(dt);
+            //
+            DBConnect.Close(conn);
+            return dt;
+
+        }
+        public bool Insert(DTO_Tram tram)
+        {
+
+            //tạo kết nối mới
+            SqlConnection conn = DBConnect.Connect();
+            //tạo command
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "newTram";
+            SqlParameter prm1 = new SqlParameter("@tenTram", tram.Ten_Tram);
+            SqlParameter prm2 = new SqlParameter("@diaDiem",tram.Dia_Diem);
+
+            cmd.Parameters.Add(prm1);
+            cmd.Parameters.Add(prm2);
+
+            int row = cmd.ExecuteNonQuery();
+            DBConnect.Close(conn);
+            if (row > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
--- GiaoDienTuyenXe/DAL/DAL_TuyenXe.cs
+++ GiaoDienTuyenXe/DAL/DAL_TuyenXe.cs
@@ -18,10 +18,8 @@ namespace DAL
             //tạo command(select *....)
             SqlCommand cmd = conn.CreateCommand();
             //khai bao se dung store cho cmd
-            //cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandType = CommandType.Text;
-            //cmd.CommandText = "ListTuyen";
-            cmd.CommandText = "select * from Tuyen";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "ListTuyen";
             //thuc thi cmd
             //khi dung datatable hoac dataset thi moi dung SQLdataAdapter
             //cmd.ExecuteNonQuery();//thuc thi tra ve so dong bi anh huong
@@ -42,10 +40,8 @@ namespace DAL
             //tạo command(select *....)
             SqlCommand cmd = conn.CreateCommand();
             //khai bao se dung store cho cmd
-            //cmd.CommandType = CommandType.StoredProcedure;
-            //cmd.CommandText = "getTenTrambyTuyen";
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select distinct(t.ID_Tuyen), tram_bat_dau.TenTram as TramBatDau, tram_ket_thuc.TenTram as TramKetThuc, tram_bat_dau.ID_Tram as IDTramBatDau, tram_ket_thuc.ID_Tram as IDTramKetThuc from Tuyen t,(select * from Tram tr) as tram_bat_dau,(select * from Tram tr) as tram_ket_thuc where t.Tram_ID_Tram1 = tram_bat_dau.ID_Tram and t.Tram_ID_Tram = tram_ket_thuc.ID_Tram";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "getTenTrambyTuyen";
                       
             //thuc thi cmd
             //khi dung datatable hoac dataset thi moi dung SQLdataAdapter
@@ -58,24 +54,50 @@ namespace DAL
             DBConnect.Close(conn);
             return dt;
         }
-        
-        public bool Them(DTO_TuyenXe tx)
+        public DataTable GetTuyenById(DTO_TuyenXe tx)
+        {
+            //tạo kết nối mới
+
+            SqlConnection conn = DBConnect.Connect();
+
+            //tạo command(select *....)
+            SqlCommand cmd = conn.CreateCommand();
+            //khai bao se dung store cho cmd
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "GetTuyenById";
+            SqlParameter prm1 = new SqlParameter("@idTuyen", tx.ID_Tuyen);
+
+            cmd.Parameters.Add(prm1);
+
+
+
+            //thuc thi cmd
+            //khi dung datatable hoac dataset thi moi dung SQLdataAdapter
+            //cmd.ExecuteNonQuery();//thuc thi tra ve so dong bi anh huong
+            SqlDataAdapter da = new SqlDataAdapter(cmd);//thuc thi tra ve table
+            DataTable dt = new DataTable();
+            //toàn bộ dữ liệu trong da se đổ (Fill) vao dt(dang table)
+            da.Fill(dt);
+            //
+            DBConnect.Close(conn);
+            return dt;
+        }
+
+        public bool Insert(DTO_TuyenXe tx)
         {
 
             //tạo kết nối mới
             SqlConnection conn = DBConnect.Connect();
             //tạo command
             SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select @idTuyen = COUNT(*) from Tuyen set @idTuyen += 1";
-            cmd.CommandText += " insert into Tuyen values(@idTuyen, @khoangCach, @thoiGianChay, @tram_id_Tram1, @tram_id_Tram2)";
-            SqlParameter prm1 = new SqlParameter("@idTuyen", tx.ID_Tuyen);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "newTuyen";
+           
             SqlParameter prm2 = new SqlParameter("@khoangCach", tx.KhoangCach);
             SqlParameter prm3 = new SqlParameter("@thoigianChay", tx.ThoiGianChay);
             SqlParameter prm4 = new SqlParameter("@tram_id_Tram1", tx.Tram_ID_Tram1);
             SqlParameter prm5 = new SqlParameter("@tram_id_Tram2", tx.Tram_ID_Tram);
 
-            cmd.Parameters.Add(prm1);
             cmd.Parameters.Add(prm2);
             cmd.Parameters.Add(prm3);
             cmd.Parameters.Add(prm4);
@@ -91,6 +113,64 @@ namespace DAL
 
 
         }
-        
+
+        public bool Delete(DTO_TuyenXe tx)
+        {
+            //tạo kết nối mới
+            SqlConnection conn = DBConnect.Connect();
+            //tạo command(select *....)
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "DeleteTuyenXe";
+            SqlParameter Param1 = new SqlParameter("@idTuyen",tx.ID_Tuyen);
+            cmd.Parameters.Add(Param1);
+
+            int row = cmd.ExecuteNonQuery();
+            DBConnect.Close(conn);
+
+            if (row > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool Update(DTO_TuyenXe tx)
+        {
+           
+            try
+            {
+                //tạo kết nối mới
+                SqlConnection conn = DBConnect.Connect();
+                //tạo command(select *....)
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "UpdateTuyenXe";
+
+
+                SqlParameter Param1 = new SqlParameter("@idTuyen", tx.ID_Tuyen);
+
+                SqlParameter Param2 = new SqlParameter("@khoangCach",tx.KhoangCach);
+                SqlParameter Param3 = new SqlParameter("@thoiGianChay", tx.ThoiGianChay);
+                SqlParameter Param4 = new SqlParameter("@tram_id_Tram1", tx.Tram_ID_Tram1);
+                SqlParameter Param5 = new SqlParameter("@tram_id_Tram2", tx.Tram_ID_Tram);
+
+
+                cmd.Parameters.Add(Param1);
+                cmd.Parameters.Add(Param2);
+                cmd.Parameters.Add(Param3);
+                cmd.Parameters.Add(Param4);
+                cmd.Parameters.Add(Param5);
+                
+                cmd.ExecuteNonQuery();
+                DBConnect.Close(conn);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }        
+        }
+
+
     }
 }
